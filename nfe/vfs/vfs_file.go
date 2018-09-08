65d2a796486bdd7ee3ea5381ddfd8375e5142ba6
Nicnl <nicnl25@gmail.com>
2018-09-09 00:41:14 +0200

    Vfs files ok

--- nfe/vfs/vfs_file.go
+++ nfe/vfs/vfs_file.go
@@ -1,15 +1,44 @@
 package vfs
 
-import "os"
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type File struct {
-	BasePath string
+	basePath string
 }
 
-func (v *File) Ls(path string) ([]string, []string) {
-	return []string{}, []string{} // Todo
+func New(basePath string) Vfs {
+	return &File{
+		basePath: strings.TrimRight(basePath, "/"),
+	}
+}
+
+func (v *File) Ls(path string) ([]string, error) {
+	path = "/" + strings.TrimLeft(path, "/")
+
+	files, err := ioutil.ReadDir(v.basePath + path)
+	if err != nil {
+		return nil, err
+	}
+
+	var out []string
+	for _, f := range files {
+		out = append(out, f.Name())
+	}
+
+	return out, nil
 }
 
 func (v *File) Stat(path string) (os.FileInfo, error) {
-	return nil, nil // Todo
+	path = "/" + strings.TrimLeft(path, "/")
+
+	stat, err := os.Stat(v.basePath + path)
+	if err != nil {
+		return nil, err
+	}
+
+	return stat, nil
 }

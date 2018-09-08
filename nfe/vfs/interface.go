56c377b0a0098e12fe077287eb73595ad9960e6b
Nicnl <nicnl25@gmail.com>
2018-09-09 00:17:25 +0200

    Ajout commentaire concernant le VFS

--- nfe/vfs/interface.go
+++ nfe/vfs/interface.go
@@ -2,6 +2,8 @@ package vfs
 
 import "os"
 
+// Virtual File System
+// Interface permettant d'accéder facilement aux fichiers système
 type Vfs interface {
 	Ls(path string) ([]string, error)
 	Stat(path string) (os.FileInfo, error)
